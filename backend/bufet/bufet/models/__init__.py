6d155b39f8834e108f4561ef974a3ce7df0014e9
morzecho <martyna.orzechowska.stud@pw.edu.pl>
2025-06-08 01:19:10 +0200

    Added some classes for views for reports in jango

--- backend/bufet/bufet/models/__init__.py
+++ backend/bufet/bufet/models/__init__.py
@@ -3,3 +3,5 @@ from .product import ProductCategory, Product, ProductStock
 from .allergen import Allergen
 from .conntact_allergen import ConntactAllergens
 from .user import UserModel
+from .daily_sales_report import DailySalesReport
+from .clients_avg_per_hour import ClientsAvgPerHour
